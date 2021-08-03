12857fa8078c918e19fa798a658f2179b9519098
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-02 23:21:37 -0500

    Setup index page; connected db and db error pages; setup css

---
+++ database_error.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" href="main.css" type="text/css">
+</head>
+
+<body>
+    <main>
+        <h1>Database Error</h1>
+        <p>There was an error communicating to the database.</p>
+        <p>The database must be installed as described in the appendix.</p>
+        <p>The database must be running as described in chapter one.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+    </main>
+</body>
+
+</html>
